32d992649915c272b760b5e010ae4808c424290a
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2020-04-27 14:58:08 +0100

    Linked binary tree implemented

---
+++ src/projectCode20280/LinkedBinaryTree.java
@@ -0,0 +1,314 @@
+package projectCode20280;
+
+/**
+ * Concrete implementation of a binary tree using a node-based, linked structure.
+ */
+public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTree<E> {
+
+
+  /** Nested static class for a binary tree node. */
+ protected static class Node<E> implements Position<E> {
+	  private E element;
+	  private Node<E> left,right,parent;
+	  
+	  public Node(E e, Node<E> p, Node<E> l, Node<E> r) {
+		  setElement(e);
+		  setParent(p);
+		  setLeft(l);
+		  setRight(r);	  
+	  }
+	  
+	  
+	public E getElement() {
+		return element;
+	}
+
+	public void setElement(E element) {
+		this.element = element;
+	}
+
+	public Node<E> getParent() {
+		return parent;
+	}
+
+	public void setParent(Node<E> parent) {
+		this.parent = parent;
+	}
+
+	public Node<E> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node<E> left) {
+		this.left = left;
+	}
+
+	public Node<E> getRight() {
+		return right;
+	}
+
+	public void setRight(Node<E> right) {
+		this.right = right;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		  sb.append(element);
+		  return sb.toString();
+	}
+  } 
+
+  /** Factory function to create a new node storing element e. */
+  protected Node<E> createNode(E e, Node<E> parent,Node<E> left, Node<E> right) {
+    return new Node<E>(e, parent, left, right);
+  }
+
+  // LinkedBinaryTree instance variables
+  /** The root of the binary tree */
+  protected Node<E> root = null;     // root of the tree
+
+  /** The number of nodes in the binary tree */
+  private int size = 0;              // number of nodes in the tree
+
+  // constructor
+  /** Construts an empty binary tree. */
+  public LinkedBinaryTree() { }      // constructs an empty binary tree
+
+  // nonpublic utility
+  /**
+   * Verifies that a Position belongs to the appropriate class, and is
+   * not one that has been previously removed. Note that our current
+   * implementation does not actually verify that the position belongs
+   * to this particular list instance.
+   *
+   * @param p   a Position (that should belong to this tree)
+   * @return    the underlying Node instance for the position
+   * @throws IllegalArgumentException if an invalid position is detected
+   */
+  protected Node<E> validate(Position<E> p) throws IllegalArgumentException {
+    if (!(p instanceof Node))
+      throw new IllegalArgumentException("Not valid position type");
+    Node<E> node = (Node<E>) p;       // safe cast
+    if (node.getParent() == node)     // our convention for defunct node
+      throw new IllegalArgumentException("p is no longer in the tree");
+    return node;
+  }
+
+  // accessor methods (not already implemented in AbstractBinaryTree)
+  /**
+   * Returns the number of nodes in the tree.
+   * @return number of nodes in the tree
+   */
+  @Override
+  public int size() {
+    return size;
+  }
+
+  /**
+   * Returns the root Position of the tree (or null if tree is empty).
+   * @return root Position of the tree (or null if tree is empty)
+   */
+  @Override
+  public Position<E> root() {
+    return root;
+  }
+
+  /**
+   * Returns the Position of p's parent (or null if p is root).
+   *
+   * @param p    A valid Position within the tree
+   * @return Position of p's parent (or null if p is root)
+   * @throws IllegalArgumentException if p is not a valid Position for this tree.
+   */
+  @Override
+  public Position<E> parent(Position<E> p) throws IllegalArgumentException {
+	  Node<E> n = validate(p);
+	  return n.getParent();
+  }
+
+  /**
+   * Returns the Position of p's left child (or null if no child exists).
+   *
+   * @param p A valid Position within the tree
+   * @return the Position of the left child (or null if no child exists)
+   * @throws IllegalArgumentException if p is not a valid Position for this tree
+   */
+  @Override
+  public Position<E> left(Position<E> p) throws IllegalArgumentException {
+	  Node<E> n = validate(p);
+	  return n.getLeft();
+  }
+
+  /**
+   * Returns the Position of p's right child (or null if no child exists).
+   *
+   * @param p A valid Position within the tree
+   * @return the Position of the right child (or null if no child exists)
+   * @throws IllegalArgumentException if p is not a valid Position for this tree
+   */
+  @Override
+  public Position<E> right(Position<E> p) throws IllegalArgumentException {
+	  Node<E> n = validate(p);
+	  return n.getRight();
+  }
+
+  // update methods supported by this class
+  /**
+   * Places element e at the root of an empty tree and returns its new Position.
+   *
+   * @param e   the new element
+   * @return the Position of the new element
+   * @throws IllegalStateException if the tree is not empty
+   */
+  public Position<E> addRoot(E e) throws IllegalStateException {
+	if(!isEmpty())throw new IllegalStateException("Tree not empty");
+	root=createNode(e, null, null, null);
+	size=1;
+	return root;
+  }
+
+  public void insert(E e){
+      //recursively add from root
+      root = addRecursive(root, e);
+      ++size;
+  }
+  
+  //recursively add Nodes to binary tree in proper position
+  private Node<E> addRecursive(Node<E> p, E e){
+	  return null;
+  }
+
+  
+  /**
+   * Creates a new left child of Position p storing element e and returns its Position.
+   *
+   * @param p   the Position to the left of which the new element is inserted
+   * @param e   the new element
+   * @return the Position of the new element
+   * @throws IllegalArgumentException if p is not a valid Position for this tree
+   * @throws IllegalArgumentException if p already has a left child
+   */
+  public Position<E> addLeft(Position<E> p, E e)throws IllegalArgumentException {
+	  Node<E> parent= validate(p);
+	  if(parent.getRight()!=null)
+		  throw new IllegalArgumentException("node has left child");
+	  Node<E> left = createNode(e, parent, null, null);
+	  parent.setLeft(left);
+	  size++;
+	  return left;
+  }
+
+  /**
+   * Creates a new right child of Position p storing element e and returns its Position.
+   *
+   * @param p   the Position to the right of which the new element is inserted
+   * @param e   the new element
+   * @return the Position of the new element
+   * @throws IllegalArgumentException if p is not a valid Position for this tree.
+   * @throws IllegalArgumentException if p already has a right child
+   */
+  public Position<E> addRight(Position<E> p, E e)throws IllegalArgumentException {
+	  Node<E> parent= validate(p);
+	  if(parent.getRight()!=null)
+		  throw new IllegalArgumentException("node has right child");
+	  Node<E> right = createNode(e, parent, null, null);
+	  parent.setRight(right);
+	  size++;
+	  return right;
+  }
+
+  /**
+   * Replaces the element at Position p with element e and returns the replaced element.
+   *
+   * @param p   the relevant Position
+   * @param e   the new element
+   * @return the replaced element
+   * @throws IllegalArgumentException if p is not a valid Position for this tree.
+   */
+  public E set(Position<E> p, E e) throws IllegalArgumentException {
+	  Node<E> node = validate(p);
+	  Node<E> old = node;
+	  node.setElement(e);
+	  return old.getElement();
+  }
+
+  /**
+   * Attaches trees t1 and t2, respectively, as the left and right subtree of the
+   * leaf Position p. As a side effect, t1 and t2 are set to empty trees.
+   *
+   * @param p   a leaf of the tree
+   * @param t1  an independent tree whose structure becomes the left child of p
+   * @param t2  an independent tree whose structure becomes the right child of p
+   * @throws IllegalArgumentException if p is not a valid Position for this tree
+   * @throws IllegalArgumentException if p is not a leaf
+   */
+  public void attach(Position<E> p, LinkedBinaryTree<E> t1,
+                    LinkedBinaryTree<E> t2) throws IllegalArgumentException {
+	//TODO
+  }
+
+  /**
+   * Removes the node at Position p and replaces it with its child, if any.
+   *
+   * @param p   the relevant Position
+   * @return element that was removed
+   * @throws IllegalArgumentException if p is not a valid Position for this tree.
+   * @throws IllegalArgumentException if p has two children.
+   */
+  public E remove(Position<E> p) throws IllegalArgumentException {
+	//TODO
+	  return null;
+  }
+  
+  public String toString() {
+	  StringBuilder sb = new StringBuilder();
+	  sb.append("[");
+	  for(Position<E> p : positions()) {
+		  sb.append(p.getElement());
+		  sb.append(", ");
+	  }
+	  sb.append("]");
+	  return sb.toString();
+  }
+  
+  public void ceateLevelOrder(E [] arr) {
+	  root = ceateLevelOrderHelper(arr,root, 0);
+  }
+  
+  private Node<E> ceateLevelOrderHelper(E [] arr, Node<E> p, int i){
+	  if(i<arr.length) {
+		  Node<E> n = createNode(arr[i], p, null, null);
+		  n.left= ceateLevelOrderHelper(arr,n.left,2*i+1);
+		  n.right= ceateLevelOrderHelper(arr,n.right,2*i+2);
+		  size++;
+		  return n; 
+	  }
+	  return p;
+  }
+  
+  public static void main(String [] args) {
+	  LinkedBinaryTree<Integer> bt = new LinkedBinaryTree<Integer>();
+//	  Position<Integer> root =bt.addRoot(12);
+//	  Position<Integer> pl= bt.addLeft(root, 25);
+//	  Position<Integer> pr= bt.addRight(root, 31);
+//	  
+//	  bt.addLeft(pl, 58);
+//	  bt.addRight(pl, 36);
+//	  
+//	  bt.addLeft(pr, 42);
+//	  bt.addRight(pr, 90);
+
+	  Integer [] arr = {12, 25, 31, 58, 36, 42, 90, 62, 75};
+	  bt.ceateLevelOrder(arr);
+	  System.out.println("bt inorder: " + bt.size() + " " + bt.inorder());
+	  System.out.println("bt preorder: " + bt.size() + " " + bt.preorder());
+	 
+	 
+	  
+//	  for(int i : arr) {
+//		  bt.insert(i);
+//	  }
+	  
+  }
+} 
+
---
+++ src/projectCode20280/LinkedDeque.java
@@ -0,0 +1,52 @@
+package projectCode20280;
+
+public class LinkedDeque<E> implements Deque<E> {
+	
+	private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	@Override
+	public E first() {
+		return list.get(0);
+	}
+
+	@Override
+	public E last() {
+		return list.get(size()-1);
+	}
+
+	@Override
+	public void addFirst(E e) {
+		list.addFirst(e);
+	}
+
+	@Override
+	public void addLast(E e) {
+		list.addLast(e);
+	}
+
+	@Override
+	public E removeFirst() {
+		return list.removeFirst();
+	}
+
+	@Override
+	public E removeLast() {
+		return list.removeLast();
+	}
+
+}
